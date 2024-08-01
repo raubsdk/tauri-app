8eeba9ccee860aef2e7b074961d4eb770386faa7
Joseph Raub <josephraub98@gmail.com>
2024-08-01 18:57:45 -0500

    Add blank app with defaults

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSameLine: false,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    useTabs: true,
+    tabWidth: 4,
+    semi: true,
+    jsxSingleQuote: false,
+    printWidth: 130,
+    proseWrap: 'always',
+    singleAttributePerLine: true,
+    endOfLine: 'lf',
+  };
+  
